367a884e4d8faf549ade1a8c5a537eaecf020143
Vedant S Kangralkar <vedant.kangralkar@betsol.com>
2026-02-13 16:21:15 +0530

    Created tables

--- app/tables.py
+++ app/tables.py
@@ -5,8 +5,31 @@ from .database import base
 class Author(base):
     __tablename__ = "authors"
 
-    id = Column(Integer, primary_key=true, index=true)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     basicinfo = Column(String)
 
-    Au
+    books = relationship("Book", back_populates="author")
+
+
+class Category(base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    
+    books = relationship("Book",back_populates="category")
+
+class Book(base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    isbn = Column(String, unique=True, nullable=True)
+    publication_year = Column(Integer, nullable=False)
+
+    author_id = Column(Integer, ForeignKey('authors.id'), nullable=False)
+    category_id = Column(Integer, ForeignKey('categories.id'), nullable=False)
+
+    author = relationship("Author", back_populates="books")
+    Category = relationship("Category", back_populates="book")
